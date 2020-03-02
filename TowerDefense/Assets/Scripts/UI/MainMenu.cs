79564f933ec14fd9ebbf9bb33c55f52430eb7e61
KonradMagiera <k.magiera1@wp.pl>
2020-03-02 16:16:55 +0100

    temporary main menu screen

---
+++ TowerDefense/Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene("Level1.1", LoadSceneMode.Single);
+    }
+
+    public void Quit() {
+        Application.Quit();
+    }
+}
