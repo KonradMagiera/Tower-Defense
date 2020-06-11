a69e1149b8b4c7f65b2111c89f23d0df45a5bc0d
KonradMagiera <k.magiera1@wp.pl>
2020-06-11 09:39:41 +0200

    healthBar indicates percentage health

--- TowerDefense/Assets/Scripts/Player.cs
+++ TowerDefense/Assets/Scripts/Player.cs
@@ -7,16 +7,20 @@ public class Player : MonoBehaviour
 {
     public static float playerHealth;
     public static float playerMoney;
+
     public float startingHealth = 30f;
     public float startingMoney = 20f;
     public TextMeshProUGUI healthBarText;
-
     public TextMeshProUGUI moneyText;
+    public GameObject healthBar; 
+
+    private RectTransform healthBarTransform;
 
     void Awake()
     {
         playerHealth = startingHealth;
         playerMoney = startingMoney;
+        healthBarTransform = healthBar.GetComponent<RectTransform>();
         UpdateStats();
         InvokeRepeating("UpdateStats", 1.0f, 0.1f);
     }
@@ -24,6 +28,9 @@ public class Player : MonoBehaviour
 
     private void UpdateStats()
     {
+        float healthPercentage = playerHealth / startingHealth;
+        healthBarTransform.localScale = new Vector3(healthPercentage, 1.0f, 1.0f);
+
         healthBarText.text = playerHealth.ToString();
         moneyText.text = playerMoney.ToString();
     }
