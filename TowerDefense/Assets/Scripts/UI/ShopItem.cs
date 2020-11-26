9966ee37d8e26385a77ae752cb20189bc73d0f0f
KonradMagiera <k.magiera1@wp.pl>
2020-11-26 13:29:26 +0100

    towers' name/price in UI

---
+++ TowerDefense/Assets/Scripts/UI/ShopItem.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class ShopItem : MonoBehaviour
+{
+    public TextMeshProUGUI towerName;
+    public TextMeshProUGUI towerPrice;
+    public GameObject tower;
+
+    void Start()
+    {
+        towerName.text = tower.name;
+        Turret target = tower.GetComponent<Turret>();
+        towerPrice.text = $"{target.price}g";
+    }
+}
