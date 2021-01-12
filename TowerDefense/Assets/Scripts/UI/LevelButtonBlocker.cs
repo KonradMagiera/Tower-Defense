0a04a336cad3263448d7d94686ed8fee5b4873e6
KonradMagiera <k.magiera1@wp.pl>
2021-01-12 11:58:08 +0100

    level buttons faded when not unlocked

---
+++ TowerDefense/Assets/Scripts/UI/LevelButtonBlocker.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelButtonBlocker : MonoBehaviour
+{
+     public string level;
+     public Button button;
+
+    void Start()
+    {
+        float playerPref = float.Parse(PlayerPrefs.GetString("currentLevel").Substring(5).Replace('.',','));
+        float givenLevel = float.Parse(level.Replace('.', ','));
+        if(playerPref >= givenLevel)
+        {
+            button.interactable = true;
+        } else
+        {
+            button.interactable = false;
+        }
+    }
+}
